978149fade9e8a5e3b0691f8889e0968c83475f8
Alexandr <Konsolka@users.noreply.github.com>
2020-03-04 16:28:56 +0300

    modified

--- norm.sh
+++ norm.sh
@@ -6,19 +6,20 @@ echo "Files/Folders that will be checked"
 ls
 echo
 ans="*"
-bool=0
+global_bool=0
 for file in $ans
 do
-	if norminette $file | grep "Error" > /dev/null; then
-		echo $file "\033[31mX\033[0m"
-		bool=1
-	else
+	if ! norminette $file | grep "Error" > /dev/null ; then
 		echo $file "\033[32mV\033[0m"
+	else
+		echo $file "\033[31mX\033[0m"
+		norminette $file
+		global_bool=1
 	fi
+	echo "------------------------"
 done
-if [ $bool -eq 1 ]; then
-	echo "\033[31m== Errors found ==\033[0m"
+if [ $global_bool -eq 1 ]; then
+	echo "\033[31m== DONE Errors found ==\033[0m"
 else
-	echo "\033[32m== No errors found ==\033[0m"
+	echo "\033[32m== DONE No errors found ==\033[0m"
 fi
-echo "===== DONE ====="
