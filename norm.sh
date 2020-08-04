639d37fa0892e18de6a2521fa7caf063995746f0
Mazie Burl <mburl@oa-b1.msk.21-school.ru>
2020-08-04 16:24:31 +0300

    some fitures

--- norm.sh
+++ norm.sh
@@ -7,13 +7,22 @@ ls
 echo
 ans="*"
 global_bool=0
+echo "Show all files in folder that didn't pass the norm?(y/n)
+\033[33m(It will be slower if there are a lot of files)\033[0m"
+read showing
 for file in $ans
 do
 	if ! norminette $file | grep "Error" > /dev/null ; then
 		echo $file "\033[32mV\033[0m"
 	else
 		echo $file "\033[31mX\033[0m"
-		norminette $file
+		case "$ans" in
+				"y"|"Y"|"Yes"|"yes")
+						norminette $file
+						;;
+				"n"|"N"|"no"|"No")
+						;;
+		esac
 		global_bool=1
 	fi
 	echo "------------------------"
